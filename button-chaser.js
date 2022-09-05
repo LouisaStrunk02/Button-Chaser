a6b31d109d924d68a10994198cb16a764b4a8aac
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-09-05 10:24:55 +0200

    add relative movement of button

--- button-chaser.js
+++ button-chaser.js
@@ -1,5 +1,7 @@
-var button = document.getElementsByClassName("button-container")[0];
+button = document.getElementsByClassName("button-container")[0];
 
 button.addEventListener("mouseover", function moveButton() {
-
+  const buttonPosLeft = button.offsetLeft;
+  button.style.left = `${buttonPosLeft + 30}px`;
+  concole.log(button.style.left);
 });
