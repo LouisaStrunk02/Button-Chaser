3daa437234c7134d082772e65aa58cda7a863612
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-09-06 10:13:21 +0200

    button jumps back to middle

--- button-chaser.js
+++ button-chaser.js
@@ -1,10 +1,10 @@
-var button = document.getElementsByClassName("button-container")[0];
+function moveButton(classname) {
+    var buttonContainer = document.getElementsByClassName(classname)[0];
 
-button.addEventListener("mouseover", function moveButton(event) {
     var mousePosX = event.clientX;
     var mousePosY = event.clientY;
-    const buttonPosX = button.offsetLeft;
-    const buttonPosY = button.offsetTop;
+    const buttonPosX = buttonContainer.offsetLeft;
+    const buttonPosY = buttonContainer.offsetTop;
 
     var mouseIsLeft = mousePosX <= buttonPosX;
     var mouseIsRight = mousePosX >= buttonPosX;
@@ -17,24 +17,34 @@ button.addEventListener("mouseover", function moveButton(event) {
     var mouseIsRightDown = mouseIsRight && mouseIsDown;
 
     if (mouseIsLeftUp) {
-        button.style.left = `${buttonPosX + 40}px`;
-        button.style.top = `${buttonPosY + 40}px`;
+        buttonContainer.style.left = `${buttonPosX + 40}px`;
+        buttonContainer.style.top = `${buttonPosY + 40}px`;
         return;
     }
     if (mouseIsRightUp) {
-        button.style.left = `${buttonPosX - 40}px`;
-        button.style.top = `${buttonPosY + 40}px`;
+        buttonContainer.style.left = `${buttonPosX - 40}px`;
+        buttonContainer.style.top = `${buttonPosY + 40}px`;
         return;
     }
     if (mouseIsLeftDown) {
-        button.style.left = `${buttonPosX + 40}px`;
-        button.style.top = `${buttonPosY - 40}px`;
+        buttonContainer.style.left = `${buttonPosX + 40}px`;
+        buttonContainer.style.top = `${buttonPosY - 40}px`;
         return;
     }
     if (mouseIsRightDown) {
-        button.style.left = `${buttonPosX - 40}px`;
-        button.style.top = `${buttonPosY - 40}px`;
+        buttonContainer.style.left = `${buttonPosX - 40}px`;
+        buttonContainer.style.top = `${buttonPosY - 40}px`;
         return;
     }
 
-});
+}
+
+function goToMiddle(classname) {
+    var button = document.getElementsByClassName(classname)[0];
+
+    button.style.left = `${document.body.offsetWidth / 2}px`;
+    button.style.top = `${document.body.offsetHeight / 2}px`;
+}
+
+document.getElementsByClassName("button-container")[0].onmouseover = () => moveButton("button-container");
+document.getElementsByClassName("button-to-chase")[0].onmouseover = () => goToMiddle("button-container");
