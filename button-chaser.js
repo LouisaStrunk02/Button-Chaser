548023ea056d696396e82802444c64136777b839
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-09-06 14:24:09 +0200

    update button-chaser

--- button-chaser.js
+++ button-chaser.js
@@ -1,39 +1,62 @@
-var button = document.getElementsByClassName("button-container")[0];
+var buttonContainer = document.getElementsByClassName("button-container")[0];
+var button = document.getElementsByClassName("button-to-chase")[0];
 
-button.addEventListener("mouseover", function moveButton(event) {
+buttonContainer.addEventListener("mouseover", function moveButton(event) {
     var mousePosX = event.clientX;
     var mousePosY = event.clientY;
-    const buttonPosX = button.offsetLeft;
-    const buttonPosY = button.offsetTop;
 
-    var mouseIsLeft = mousePosX <= buttonPosX;
-    var mouseIsRight = mousePosX >= buttonPosX;
-    var mouseIsUp = mousePosY <= buttonPosY;
-    var mouseIsDown = mousePosY >= buttonPosY;
+    var buttonHeight = button.offsetHeight;
+    var buttonWidth = button.offsetWidth;
 
-    var mouseIsLeftUp = mouseIsLeft && mouseIsUp;
-    var mouseIsRightUp = mouseIsRight && mouseIsUp;
-    var mouseIsLeftDown = mouseIsLeft && mouseIsDown;
-    var mouseIsRightDown = mouseIsRight && mouseIsDown;
+    var buttonLeft = buttonContainer.offsetLeft + button.offsetLeft;
+    var buttonRight = buttonLeft + buttonWidth;
+    var buttonTop = buttonContainer.offsetTop + button.offsetTop;
+    var buttonBottom = buttonTop + buttonHeight;
 
+    var mouseIsLeft = mousePosX <= buttonLeft && mousePosY >= buttonTop && mousePosY <= buttonBottom;
+    var mouseIsRight = mousePosX >= buttonRight && mousePosY >= buttonTop && mousePosY <= buttonBottom;
+    var mouseIsUp = mousePosX >= buttonLeft && mousePosX <= buttonRight && mousePosY <= buttonTop;
+    var mouseIsDown = mousePosX >= buttonLeft && mousePosX <= buttonRight && mousePosY >= buttonBottom;
+
+    var mouseIsLeftUp = mousePosX <= buttonLeft && mousePosY <= buttonTop;
+    var mouseIsRightUp = mousePosX >= buttonRight && mousePosY <= buttonTop;
+    var mouseIsLeftDown = mousePosX <= buttonLeft && mousePosY >= buttonBottom;
+    var mouseIsRightDown = mousePosX >= buttonRight && mousePosY >= buttonBottom;
+
+    if (mouseIsLeft) {
+        buttonContainer.style.left = `${buttonContainer.offsetLeft + 40}px`;
+        return;
+    }
+    if (mouseIsRight) {
+        buttonContainer.style.left = `${buttonContainer.offsetLeft - 40}px`;
+        return;
+    }
+    if (mouseIsUp) {
+        buttonContainer.style.top = `${buttonContainer.offsetTop + 40}px`;
+        return;
+    }
+    if (mouseIsDown) {
+        buttonContainer.style.top = `${buttonContainer.offsetTop - 40}px`;
+        return;
+    }
     if (mouseIsLeftUp) {
-        button.style.left = `${buttonPosX + 40}px`;
-        button.style.top = `${buttonPosY + 40}px`;
+        buttonContainer.style.left = `${buttonContainer.offsetLeft + 40}px`;
+        buttonContainer.style.top = `${buttonContainer.offsetTop + 40}px`;
         return;
     }
     if (mouseIsRightUp) {
-        button.style.left = `${buttonPosX - 40}px`;
-        button.style.top = `${buttonPosY + 40}px`;
+        buttonContainer.style.left = `${buttonContainer.offsetLeft - 40}px`;
+        buttonContainer.style.top = `${buttonContainer.offsetTop + 40}px`;
         return;
     }
     if (mouseIsLeftDown) {
-        button.style.left = `${buttonPosX + 40}px`;
-        button.style.top = `${buttonPosY - 40}px`;
+        buttonContainer.style.left = `${buttonContainer.offsetLeft + 40}px`;
+        buttonContainer.style.top = `${buttonContainer.offsetTop - 40}px`;
         return;
     }
     if (mouseIsRightDown) {
-        button.style.left = `${buttonPosX - 40}px`;
-        button.style.top = `${buttonPosY - 40}px`;
+        buttonContainer.style.left = `${buttonContainer.offsetLeft - 40}px`;
+        buttonContainer.style.top = `${buttonContainer.offsetTop - 40}px`;
         return;
     }
 });
